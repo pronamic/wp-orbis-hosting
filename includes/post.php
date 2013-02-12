ed5f257adca7c1fbd177e33f846db703f5ce19fe
Remco Tolsma <info@remcotolsma.nl>
2013-02-12 13:26:55 +0100

    Add hosting group post type.

--- includes/post.php
+++ includes/post.php
@@ -4,6 +4,25 @@ function orbis_hosting_create_initial_post_types() {
 	global $orbis_hosting_plugin;
 
 	register_post_type(
+		'orbis_hosting_group',
+		array(
+			'label'         => __( 'Hosting Group', 'orbis' ),
+				'labels'        => array(
+				'name'          => __( 'Hosting Groups', 'orbis' ),
+				'singular_name' => __( 'Hosting Group', 'orbis' ),
+				'add_new'       => _x( 'Add New', 'hosting_group', 'orbis' ),
+				'add_new_item'  => __( 'Add New Hosting Group', 'orbis' )
+			),
+			'public'        => true,
+			'menu_position' => 30,
+			'menu_icon'     => plugins_url( 'images/hosting_group.png', $orbis_hosting_plugin->file ),
+			'supports'      => array( 'title', 'comments' ),
+			'has_archive'   => true,
+			'rewrite'       => array( 'slug' => 'hostinggroep' )
+		)
+	);
+
+	register_post_type(
 		'orbis_domain_name',
 		array(
 			'label'         => __( 'Domain Names', 'orbis' ),
