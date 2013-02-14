638fc66d30f7c76bd63e475ef39bf8a57850361c
Remco Tolsma <info@remcotolsma.nl>
2013-02-14 17:35:36 +0100

    Added support for Posts2Posts for hosting groups and domain names.

--- includes/post.php
+++ includes/post.php
@@ -43,3 +43,53 @@ function orbis_hosting_create_initial_post_types() {
 }
 
 add_action( 'init', 'orbis_hosting_create_initial_post_types', 0 ); // highest priority
+
+function orbis_hosting_p2p_init() {
+	p2p_register_connection_type( array(
+		'name'        => 'orbis_hosting_groups_to_domain_names',
+		'from'        => 'orbis_hosting_group',
+		'to'          => 'orbis_domain_name',
+		'cardinality' => 'one-to-many'
+	) );
+}
+
+add_action( 'p2p_init', 'orbis_hosting_p2p_init' );
+
+/**
+ * Hosting group content
+*/
+function orbis_hosting_the_content( $content ) {
+	if ( get_post_type() == 'orbis_hosting_group' ) {
+		$id = get_the_ID();
+
+		$domain_names = get_posts( array(
+			'connected_type'  => 'orbis_hosting_groups_to_domain_names',
+			'connected_items' => get_queried_object(),
+			'nopaging'        => true
+		) );
+
+		if ( ! empty( $domain_names ) ) {
+			$str  = '';
+			
+			$str .= '<ul>';
+			
+			foreach ( $domain_names as $domain_name ) {
+				$str .= '<li>';
+				$str .= sprintf(
+					'<a href="%s">%s</a>',
+					get_permalink( $domain_name ),
+					get_the_title( $domain_name )
+				);
+				$str .= '</li>';
+			}
+			
+			$str .= '</ul>';
+			
+			$content .= $str;
+		}
+	}
+
+	return $content;
+}
+
+add_filter( 'the_content', 'orbis_hosting_the_content' );
