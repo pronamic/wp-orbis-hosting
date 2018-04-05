5a70482aef4caa18bd51e92b9b6f72767e2db10a
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-05 11:50:29 +0200

    Changed Bootstrap card-title to card-header.

--- templates/domain-name-hosting-group-details.php
+++ templates/domain-name-hosting-group-details.php
@@ -5,8 +5,8 @@ $hosting_groups_query = p2p_type( 'orbis_hosting_groups_to_domain_names' )->set_
 while ( $hosting_groups_query->have_posts() ) : $hosting_groups_query->the_post(); ?>
 
 	<div class="card">
+		<div class="card-header"><?php esc_html_e( 'Hosting Group', 'orbis_hosting' ); ?></div>
 		<div class="card-body">
-			<h3 class="card-title"><?php esc_html_e( 'Hosting Group', 'orbis_hosting' ); ?></h3>
 
 			<div class="content">
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
